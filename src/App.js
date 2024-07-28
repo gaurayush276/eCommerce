a4cbcb9e6ca3412c7c688d88ec39e233ad13534f
Ayush <gaurayush276@gmail.com>
2024-07-28 18:16:59 +0530

    Added routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
  import './App.css';
 import Home from './pages/Home';
+ import LoginPage from './pages/LoginPage';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+ 
 
 function App() {
+
+  const appRouter = createBrowserRouter([
+    {
+      path : '/login',
+      element: <LoginPage/>
+    },
+    {
+      path :'/' ,
+      element : <Home/>
+    }
+
+  ])
   return (
     <div >
-    <Home/>
+     <RouterProvider  router={appRouter}
+     >
+      <Outlet/>
+     </RouterProvider>
     </div>
   );
 }
